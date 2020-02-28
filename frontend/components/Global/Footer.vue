7304209c4fee69388b591bf3571d095c06b397fd
PoLLi <pollibeatz@googlemail.com>
2020-02-28 14:23:43 +0100

    Home view update
    Basic Theme Design
    + added Navbar Component
    + added Footer Component
    + added Theme SCSS Styles

---
+++ frontend/components/Global/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <footer class="footer mt-auto pt-3">
+            <b-container>
+                <p class="text-muted text-right">Copyright © 2020 PoLLi | Part of Just-Look Network</p>
+            </b-container>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
