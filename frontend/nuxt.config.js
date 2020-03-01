73d22900b58ff1f36c7a5b0167cea1b86ebd096f
PoLLi <pollibeatz@googlemail.com>
2020-03-01 13:27:33 +0100

    vue-fontawesome component setup

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -46,7 +46,7 @@ export default {
     /*
      ** Plugins to load before mounting the App
      */
-    plugins: [],
+    plugins: ['~/plugins/vue-fontawesome.js'],
 
     /*
      ** Nuxt.js dev-modules
