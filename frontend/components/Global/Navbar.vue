ca706b9a04a24061d962d1511218fa1c3038dc58
PoLLi <pollibeatz@googlemail.com>
2020-03-01 14:24:30 +0100

    update Navbar
    set nav link pros
    added link icons

--- frontend/components/Global/Navbar.vue
+++ frontend/components/Global/Navbar.vue
@@ -8,9 +8,26 @@
             <b-collapse id="nav-collapse" is-nav>
                 <!-- Right aligned nav items -->
                 <b-navbar-nav class="ml-auto">
-                    <b-nav-item href="#" active>Home</b-nav-item>
-                    <b-nav-item href="#">Logo Generator</b-nav-item>
-                    <b-nav-item href="#">URL Shortener</b-nav-item>
+                    <b-nav-item to="/" exact>Home</b-nav-item>
+                    <b-nav-item to="/logo">Logo Generator</b-nav-item>
+                    <b-nav-item to="/url">URL Shortener</b-nav-item>
+                    <b-nav-item
+                        href="https://github.com/Just-Look-Network/JL_WebTools/"
+                        target="_blank"
+                        class="nav-icon"
+                    >
+                        <font-awesome-icon :icon="['fab', 'github']" />
+                    </b-nav-item>
+                    <b-nav-item href="http://discord.just-look.net/" target="_blank" class="nav-icon">
+                        <font-awesome-icon :icon="['fab', 'discord']" />
+                    </b-nav-item>
+                    <b-nav-item
+                        href="https://twitter.com/justlooknetwork"
+                        target="_blank"
+                        class="nav-icon nav-icon-last"
+                    >
+                        <font-awesome-icon :icon="['fab', 'twitter']" />
+                    </b-nav-item>
                 </b-navbar-nav>
             </b-collapse>
         </b-container>
