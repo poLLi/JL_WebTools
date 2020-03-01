b67c2af1c1d3db910ec3461f57d46f5b733fb97d
PoLLi <pollibeatz@googlemail.com>
2020-03-01 14:57:01 +0100

    url page placeholder

--- frontend/pages/url.vue
+++ frontend/pages/url.vue
@@ -2,27 +2,23 @@
     <div>
         <section class="first-section">
             <b-container>
-                <b-row>
+                <b-row align-h="center">
                     <b-col lg="8" class="text-center">
-                        asdasd
+                        <h1 class="mt-5">NOPE!</h1>
                     </b-col>
-                    <b-col lg="4" class="text-center">
-                        <div class="card">
-                            <div class="btn-group">
-                                adsasd
-                            </div>
+                </b-row>
+            </b-container>
+        </section>
 
-                            <div
-                                class="card-body text-center d-flex justify-content-center align-items-center flex-column"
-                            >
-                                <p>Choose your Color:</p>
-                                <client-only>
-                                    asdasd
-                                </client-only>
-                                <br />
-                                <button type="button" class="btn btn-dark btn-block">Export</button>
-                            </div>
-                        </div>
+        <section class="page-section bg-dark">
+            <b-container>
+                <b-row align-h="center">
+                    <b-col lg="8" class="text-center">
+                        <h2 class="text-white mt-0">URL Shortener</h2>
+                        <hr class="divider light my-4" />
+                        <p class="text-white">
+                            is comming soon!
+                        </p>
                     </b-col>
                 </b-row>
             </b-container>
