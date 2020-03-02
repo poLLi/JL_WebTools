d9f2a62af56e9ceffc9790b7925b7844d6819445
PoLLi <pollibeatz@googlemail.com>
2020-03-02 11:55:45 +0100

    Update nuxt.config.js

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -2,7 +2,7 @@ const routerBase =
     process.env.DEPLOY_ENV === 'GH_PAGES'
         ? {
               router: {
-                  base: '/JL_WebTools/'
+                  base: '/'
               }
           }
         : {
